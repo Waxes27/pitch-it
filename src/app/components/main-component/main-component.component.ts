a8fdef755349190f24c6c6a0a91bfc997974186b
Waxes27 <nceba.dumasi@gmail.com>
2023-02-18 00:13:30 +0200

    landing page done (login link added)

--- src/app/components/main-component/main-component.component.ts
+++ src/app/components/main-component/main-component.component.ts
@@ -11,7 +11,7 @@ export class MainComponentComponent implements OnInit {
   constructor(public auth: AuthService,private router: Router) {
     
   }
-  routesList = ["/","/questionaire","/login","/register/investor","/preferences/investor",]
+  routesList = ["/","/questionaire","/login","/register/investor","/preferences/investor","/register/business","/preferences/business"]
   checkRoute(){
     
     for (var key in this.routesList) {
