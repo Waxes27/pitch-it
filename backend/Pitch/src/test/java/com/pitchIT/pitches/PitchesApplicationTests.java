d4ae73d520f180f5b2af2cf3d77306ea4bf98d7c
Waxes27 <nceba.dumasi@gmail.com>
2023-03-29 21:13:28 +0200

    Pitches module

---
+++ backend/Pitch/src/test/java/com/pitchIT/pitches/PitchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitchIT.pitches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
