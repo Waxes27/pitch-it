39c13d23b7cb0998e01dbd1495cbc5f743a1b1c9
Waxes27 <nceba.dumasi@gmail.com>
2023-03-30 20:58:04 +0200

    Dev Pipeline done

--- backend/scripts/reinit-db.sh
+++ backend/scripts/reinit-db.sh
@@ -1,5 +1,5 @@
 docker-compose down
 docker system prune -a --volumes -f
-docker-compose up -d
-docker exec -it backend_postgres_1 bash
+#docker-compose up -d
+#docker exec -it backend_postgres_1 bash
 
