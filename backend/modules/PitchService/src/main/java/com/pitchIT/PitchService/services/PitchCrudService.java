5ed4a0d8f34777038ad56b3042090a27e58ce9b6
Waxes27 <nceba.dumasi@gmail.com>
2023-03-31 00:30:41 +0200

    PitchService: Major changes done to services

--- backend/modules/PitchService/src/main/java/com/pitchIT/PitchService/services/PitchCrudService.java
+++ backend/modules/PitchService/src/main/java/com/pitchIT/PitchService/services/PitchCrudService.java
@@ -1,2 +1,52 @@
-package com.pitchIT.PitchService.services;public class PitchCrudService {
+package com.pitchIT.PitchService.services;
+
+import com.pitchIT.PitchService.models.*;
+import com.pitchIT.PitchService.models.socials.Socials;
+import com.pitchIT.PitchService.repositories.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+
+public class PitchCrudService {
+    // Repositories Here
+    private final PitchRepository pitchRepository;
+    private final BusinessDetailsRepository businessDetailsRepository;
+    private final CompanyDetailsRepository companyDetailsRepository;
+    private final DocumentsRepository documentsRepository;
+    private final MarketDetailsRepository marketDetailsRepository;
+    private final MemberDetailsRepository memberDetailsRepository;
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    public void savePitch(
+            BusinessDetails businessDetails,
+            CompanyDetails companyDetails,
+            Documents documents,
+            MarketDetails marketDetails,
+            MemberDetails memberDetails
+
+    ){
+
+        Pitch pitch = Pitch.builder()
+            .businessDetails(businessDetailsRepository.save(businessDetails))
+            .companyDetails(companyDetailsRepository.save(companyDetails))
+            .documents(documentsRepository.save(documents))
+            .marketDetails(marketDetailsRepository.save(marketDetails))
+            .memberDetails(memberDetailsRepository.save(memberDetails))
+            .build();
+
+
+        pitchRepository.save(pitch);
+
+
+    }
+
+
 }
