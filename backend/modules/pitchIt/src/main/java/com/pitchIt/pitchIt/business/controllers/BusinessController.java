62dbaec60f6d375b53b8015fe73c449305ea842f
tmoletsa <tmoletsa@student.wethinkcode.co.za>
2023-03-30 21:19:22 +0200

    UserService: Cors Configuration

--- backend/modules/pitchIt/src/main/java/com/pitchIt/pitchIt/business/controllers/BusinessController.java
+++ backend/modules/pitchIt/src/main/java/com/pitchIt/pitchIt/business/controllers/BusinessController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/business")
-// Add "/api" path here
+@CrossOrigin
 public class BusinessController {
     private final BusinessRepository repository;
 
---
+++ backend/modules/pitchIt/src/main/java/com/pitchIt/pitchIt/security/cors/CorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.pitchIt.pitchIt.security.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
