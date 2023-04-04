b0c35e6319d5fa85fa7dbbc12f88f522dd3c5ea6
Waxes27 <nceba.dumasi@gmail.com>
2023-04-04 03:08:15 +0200

    PitchUserService: User can fetch multiple Pitches

--- backend/modules/PitchUserService/src/test/java/com/pitchIT/PitchUserService/PitchPitchBusinessUserServiceApplicationTests.java
+++ backend/modules/PitchUserService/src/test/java/com/pitchIT/PitchUserService/PitchPitchBusinessUserServiceApplicationTests.java
@@ -3,11 +3,12 @@ package com.pitchIT.PitchUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PitchPitchBusinessUserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
