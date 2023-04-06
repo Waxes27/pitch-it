a8e904a8c3310e8a944a901cfb8eecf24dd9b654
Waxes27 <nceba.dumasi@gmail.com>
2023-04-06 03:23:34 +0200

    Compiling full site

--- backend/modules/pitchIt/src/main/java/com/pitchIt/pitchIt/preference/repositories/UserPrefencesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pitchIt.pitchIt.preference.repositories;
-
-import com.pitchIt.pitchIt.preference.entities.UserPreference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPrefencesRepository extends JpaRepository<UserPreference, Long> {
-
-
-}
