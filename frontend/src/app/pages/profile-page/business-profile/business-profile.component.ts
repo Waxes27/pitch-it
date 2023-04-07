774765ee82835b939e90d528e3f79c7f9f7bdbac
Babahle <mokonebabzini@gmail.com>
2023-04-07 09:47:31 +0200

    creating about info for user

--- frontend/src/app/pages/profile-page/business-profile/business-profile.component.ts
+++ frontend/src/app/pages/profile-page/business-profile/business-profile.component.ts
@@ -1,21 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { UserModel } from "src/app/models/User";
 
 @Component({
-  selector: 'app-business-profile',
-  templateUrl: './business-profile.component.html',
-  styleUrls: ['./business-profile.component.sass']
+  selector: "app-business-profile",
+  templateUrl: "./business-profile.component.html",
+  styleUrls: ["./business-profile.component.sass"],
 })
 export class BusinessProfileComponent implements OnInit {
   readMore: boolean = true;
-  constructor() { }
+  creatingAbout: boolean = false;
+  user: UserModel = new UserModel();
+  aboutForm = new FormGroup({
+    about: new FormControl("", [<any>Validators.required]),
+  });
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get<UserModel>("http://localhost:8081/user/a@gmail.com")
+      .subscribe((data) => {
+        this.user = data;
+      });
   }
 
-  readMoreClick(){
+  readMoreClick() {
     this.readMore = !this.readMore;
     console.log(`Read More > ${this.readMore}`);
-    
   }
 
+  createAbout() {
+    this.creatingAbout = !this.creatingAbout;
+  }
+
+  onSubmit() {
+    let formData = {
+      about: this.aboutForm.get("about")?.value,
+    };
+    this.user.setAbout(formData.about!);
+    this.creatingAbout = !this.creatingAbout;
+  }
 }
