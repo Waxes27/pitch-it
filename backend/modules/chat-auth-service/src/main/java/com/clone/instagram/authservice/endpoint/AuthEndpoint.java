a6cb3549288870e1dacb09b925b50c31ea30db6e
Waxes27 <nceba.dumasi@gmail.com>
2023-04-07 15:13:59 +0200

    CORS Issues

--- backend/modules/chat-auth-service/src/main/java/com/clone/instagram/authservice/endpoint/AuthEndpoint.java
+++ backend/modules/chat-auth-service/src/main/java/com/clone/instagram/authservice/endpoint/AuthEndpoint.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ public class AuthEndpoint {
     @Autowired private UserService userService;
     @Autowired private FacebookService facebookService;
 
+    @CrossOrigin
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         System.out.println(loginRequest.getUsername());
@@ -43,6 +45,7 @@ public class AuthEndpoint {
         return ResponseEntity.ok(new JwtAuthenticationResponse(token));
     }
 
+    @CrossOrigin
     @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createUser(@Valid @RequestBody SignUpRequest payload) {
         log.info("creating user {}", payload.getUsername());
