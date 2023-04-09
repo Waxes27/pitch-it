6cb492fa977d88e07b245cc7920b79fe823080a8
tmoletsa <tmoletsa@student.wethinkcode.co.za>
2023-04-09 21:00:14 +0200

    Added headers in WebSecurityConfig::corsFilter

--- backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSecurityConfig.java
+++ backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSecurityConfig.java
@@ -9,17 +9,23 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
 
     @Bean
     public CorsFilter corsFilter() {
+
+        List<String> headers = new ArrayList<>();
+        headers.add("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedHeaders(headers);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
