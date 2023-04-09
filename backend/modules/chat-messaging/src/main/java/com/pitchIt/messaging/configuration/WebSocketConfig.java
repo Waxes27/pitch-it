1c7a0c1899a523c221b694021c152abc854cb3ea
tmoletsa <tmoletsa@student.wethinkcode.co.za>
2023-04-09 18:49:09 +0200

    Chat.js: ip fix

--- backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSocketConfig.java
+++ backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
         .addEndpoint("/ws")
-        .setAllowedOrigins("http://localhost:3000")
+        .setAllowedOrigins("http://102.221.36.216:3000")
         .withSockJS();
     }
 
