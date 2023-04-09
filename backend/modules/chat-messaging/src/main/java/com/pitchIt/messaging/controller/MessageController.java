2b4652b7dd28fcfd2c2dc4af13cf4510cfc3fada
tmoletsa <tmoletsa@student.wethinkcode.co.za>
2023-04-09 18:59:31 +0200

    Attempting Cors fix

--- backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/controller/MessageController.java
+++ backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/controller/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pitchIt.messaging.model.ChatMessage;
@@ -16,7 +17,7 @@ import com.pitchIt.messaging.service.ChatMessageService;
 import com.pitchIt.messaging.service.ChatRoomService;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class MessageController {
     
     @Autowired private SimpMessagingTemplate messagingTemplate;
