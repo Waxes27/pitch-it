1b3f0c3d8befd229651408198c6b9b58ac100056
tmoletsa <tmoletsa@student.wethinkcode.co.za>
2023-04-10 11:44:02 +0200

    Fixing CORS

--- backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSecurityConfig.java
+++ backend/modules/chat-messaging/src/main/java/com/pitchIt/messaging/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.pitchIt.messaging.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,6 +28,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
         return new CorsFilter(source);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .cors().disable()
+        .authorizeRequests()
+        .anyRequest()
+        .permitAll();
+
+    }
+
 //    @Bean
 //    public WebMvcConfigurer corsConfigurer() {
 //    return new WebMvcConfigurer() {
@@ -42,6 +54,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 //}
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedMethods("*");
+        registry.addMapping("/*")
+        .allowedOrigins("http://102.221.36.216:3000")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(false);
     }
 }
