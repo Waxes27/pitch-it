4a81e9e3c0a4678b4d4de02c34a5f9a50cbab228
Waxes27 <nceba.dumasi@gmail.com>
2023-04-13 16:34:32 +0200

    Adding endpoint Pitch by ID

--- backend/modules/PitchUserService/src/main/java/com/pitchIT/PitchUserService/controllers/PitchController.java
+++ backend/modules/PitchUserService/src/main/java/com/pitchIT/PitchUserService/controllers/PitchController.java
@@ -5,6 +5,7 @@ import com.pitchIT.PitchUserService.models.Pitch;
 import com.pitchIT.PitchUserService.requests.PitchDetailsRequest;
 import com.pitchIT.PitchUserService.services.UserService;
 import lombok.AllArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -44,10 +45,10 @@ public class PitchController {
     }
 
     @GetMapping("id")
-    public ResponseEntity<Pitch> getPitchById(@RequestParam String id){
+    public ResponseEntity<Object> getPitchById(@RequestParam String id){
         return restTemplate.getForEntity(
                 "http://pitch-service:10001/api/pitch/id?id="+id,
-                Pitch.class);
+                Object.class);
     }
 
 
