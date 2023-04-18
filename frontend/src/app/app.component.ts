63cc537fe7d7dca54e44750348e5657c59c26857
Babahle <mokonebabzini@gmail.com>
2023-04-18 22:10:59 +0200

    fix background

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
