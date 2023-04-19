4b986e6963baa542d964db2d48fc5d824b86aff0
Waxes27 <nceba.dumasi@gmail.com>
2023-04-19 16:30:13 +0200

    Dockerize Frontend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyChKENBu1yT5mK1z90MfY7LvPWtop3uHnQ",
+        authDomain: "pitchit-testing.firebaseapp.com",
+        projectId: "pitchit-testing",
+        storageBucket: "pitchit-testing.appspot.com",
+        messagingSenderId: "706732041532",
+        appId: "1:706732041532:web:2abcfaf7e70b33e3d26a39"
+    }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
