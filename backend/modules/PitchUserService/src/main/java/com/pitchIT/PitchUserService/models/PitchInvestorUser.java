4a8d19b3629ec0d52cd84910c1c4d82573906e8c
Waxes27 <nceba.dumasi@gmail.com>
2023-04-22 21:56:25 +0200

    The Lords Work

--- backend/modules/PitchUserService/src/main/java/com/pitchIT/PitchUserService/models/PitchInvestorUser.java
+++ backend/modules/PitchUserService/src/main/java/com/pitchIT/PitchUserService/models/PitchInvestorUser.java
@@ -31,9 +31,7 @@ public class PitchInvestorUser implements UserDetails {
     private String pictureUrl;
     private String password;
     private String about;
-
     private boolean paid = false;
-
 
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "investment_history_id")
