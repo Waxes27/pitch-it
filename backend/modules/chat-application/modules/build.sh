e2f565e5dc22b04333c28da03ec9cd74ebb7eef7
Waxes27 <nceba.dumasi@gmail.com>
2023-05-01 21:03:20 +0200

    Testing JWT Token fetching

--- backend/modules/chat-application/modules/build.sh
+++ backend/modules/chat-application/modules/build.sh
@@ -1,3 +1,3 @@
-cd chat-auth-service && mvn clean install -DskipTests
+cd chat-auth-service && mvn clean install
 
-cd ../chat-messaging && mvn clean install -DskipTests
+cd ../chat-messaging && mvn clean install
