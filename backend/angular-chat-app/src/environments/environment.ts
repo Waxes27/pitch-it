5c6e1f6644e0c839bf3601defb7dae04f40b5aeb
Waxes27 <nceba.dumasi@gmail.com>
2023-05-10 09:21:15 +0200

    Chat Firestore link with backend

---
+++ backend/angular-chat-app/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD626vU2wzkGeTYMuLXuYy8TCDpPTGCijQ",
+    authDomain: "chat-4c31c.firebaseapp.com",
+    databaseURL: "https://chat-4c31c-default-rtdb.firebaseio.com",
+    projectId: "chat-4c31c",
+    storageBucket: "chat-4c31c.appspot.com",
+    messagingSenderId: "405362483895",
+    appId: "1:405362483895:web:df6eaaaf4821a6eeb1e332",
+    measurementId: "G-D0KXJ5HEQM"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
